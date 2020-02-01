3c5c5e76fb26b10ea0c6b9c03839a1b0b19c9a0d
Kamil Porwit <kamilporwit93@gmail.com>
2020-02-02 00:26:24 +0900

    add while loop to raw_input
    Now the program will execute till user input will be proper word. Need
    to work on the ext signal handling though.

--- hangman.py
+++ hangman.py
@@ -2,9 +2,12 @@ def word_check(given_word):
     given_word = given_word.lower()
     for letter in given_word:
         if(letter.isalpha() == False):
-            print "Word must conatin only letters"
+            print "Word must conatin only letters. Try again."
             return 1
+        else:
+            return 0
 
-
-word = raw_input("Define word for guessing: ")
-word_check(word)
+word_check_return = 1
+while word_check_return != 0:
+    word = raw_input("Define word for guessing: ")
+    word_check_return = word_check(word)
