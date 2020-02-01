d0aa3ccb8bc80ec74e78a654ac438002790d8478
Kamil Porwit <kamilporwit93@gmail.com>
2020-02-02 00:19:59 +0900

    change word check to isalpha method
    Previous word_check function contained two methods for checking if the
    given word is proper. With isalpha() method only one if is used.

--- hangman.py
+++ hangman.py
@@ -1,13 +1,10 @@
 def word_check(given_word):
     given_word = given_word.lower()
     for letter in given_word:
-        if letter.isspace():
-            print "Given word cannot contain spaces"
+        if(letter.isalpha() == False):
+            print "Word must conatin only letters"
             return 1
-        if letter.isdigit():
-            print "Given word cannot contain numeric characters"
-            return 2
 
 
 word = raw_input("Define word for guessing: ")
-
+word_check(word)
